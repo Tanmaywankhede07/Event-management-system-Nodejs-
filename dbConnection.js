b70a601fafee08738e3fe747567bfcb4e6dde508
Tanmaywankhede07 <wankhede_tanmay.cs@ghrce.raisoni.net>
2022-12-13 23:46:37 +0530

    Registeration done

---
+++ dbConnection.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'tanmay',
+  password: 'Tanmay@123',
+  database: 'event_management' 
+}); 
+ 
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log('Database is connected successfully !');
+});
+module.exports = conn;
